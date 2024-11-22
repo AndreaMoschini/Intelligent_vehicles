4a00e43bfb4ee62fc79b691733068250455e55e9
AndreaMoschini <andrea.moschini@studenti.unitn.it>
2024-11-22 10:13:34 +0100

    better repo setup

---
+++ Test2/basic_agent_st/starting_point.cc
@@ -0,0 +1,136 @@
+//
+// Created by Gastone Pietro Rosati Papini on 10/08/22.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+
+extern "C" {
+#include "screen_print_c.h"
+}
+#include "screen_print.h"
+#include "server_lib.h"
+#include "logvars.h"
+
+// --- MATLAB PRIMITIVES INCLUDE ---
+// #include "primitives.h"
+// --- MATLAB PRIMITIVES INCLUDE ---
+
+#define DEFAULT_SERVER_IP    "127.0.0.1"
+#define SERVER_PORT               30000  // Server port
+#define DT 0.05
+
+// Handler for CTRL-C
+#include <signal.h>
+static uint32_t server_run = 1;
+void intHandler(int signal) {
+    server_run = 0;
+}
+
+int main(int argc, const char * argv[]) {
+    logger.enable(true);
+
+    // Messages variables
+    scenario_msg_t scenario_msg;
+    manoeuvre_msg_t manoeuvre_msg;
+    size_t scenario_msg_size = sizeof(scenario_msg.data_buffer);
+    size_t manoeuvre_msg_size = sizeof(manoeuvre_msg.data_buffer);
+    uint32_t message_id = 0;
+
+#if not defined( _MSC_VER ) and not defined( _WIN32 )
+    // More portable way of supporting signals on UNIX
+    struct sigaction act;
+    act.sa_handler = intHandler;
+    sigaction(SIGINT, &act, NULL);
+#else
+    signal(SIGINT, intHandler);
+#endif
+
+    server_agent_init(DEFAULT_SERVER_IP, SERVER_PORT);
+
+    // Start server of the Agent
+    printLine();
+    printTable("Waiting for scenario message...", 0);
+    printLine();
+    while (server_run == 1) {
+
+        // Clean the buffer
+        memset(scenario_msg.data_buffer, '\0', scenario_msg_size);
+
+        // Receive scenario message from the environment
+        if (server_receive_from_client(&server_run, &message_id, &scenario_msg.data_struct) == 0) {
+            // Init time
+            static auto start = std::chrono::system_clock::now();
+            auto time = std::chrono::system_clock::now()-start;
+            double num_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(time).count()/1000.0;
+            printLogTitle(message_id, "received message");
+
+            // Data struct
+            // we take data from scenario and save it into the variable in
+            input_data_str *in = &scenario_msg.data_struct;
+            manoeuvre_msg.data_struct.CycleNumber = in->CycleNumber;
+            manoeuvre_msg.data_struct.Status = in->Status;
+
+
+            static double init_dist = in->TrfLightDist;
+            double dist = init_dist - in->TrfLightDist;
+            // double a0 = in->ALgtFild; // longitudinal acceleration
+
+            const double maxACC = 5;
+            const double minACC = -5;
+            double a0 = fmin(fmax(in->ALgtFild, minACC), maxACC); // longitudinal acceleration saturated
+
+            // Example of using log
+            logger.log_var("Example", "cycle", in->CycleNumber);
+            logger.log_var("Example", "vel", in->VLgtFild);
+            logger.log_var("Example", "time", in->ECUupTime); 
+            logger.log_var("Example", "dist", dist); 
+
+
+            // ADD AGENT CODE HERE
+
+            const double req_acc = 13; // target acc
+            static double req_vel = 0; // static = initialized only the first time to 0 and the modified every step
+            req_vel = req_vel + req_acc*DT;
+
+            // PID CONTROL
+            double P_gain = 0.02;
+            double I_gain = 0.01;
+            static double integral = 0;
+            double error = req_acc - a0;
+            integral = integral + error*DT;
+            double req_ped = P_gain*error + I_gain*integral;
+
+            logger.log_var("Example", "req_acc", req_acc);
+            logger.log_var("Example", "req_vel", req_vel); 
+            logger.log_var("Example", "req_ped", req_ped);
+
+            // ADD LOW LEVEL CONTROL CODE HERE
+            manoeuvre_msg.data_struct.RequestedAcc = req_ped; //RequestedAcc stand for gas pedal in this case
+            manoeuvre_msg.data_struct.RequestedSteerWhlAg = 0.0;
+
+            // Write log
+            logger.write_line("Example");
+
+            // Screen print
+            printLogVar(message_id, "Time", num_seconds);
+            printLogVar(message_id, "Status", in->Status);
+            printLogVar(message_id, "CycleNumber", in->CycleNumber);
+
+            // Send manoeuvre message to the environment
+            if (server_send_to_client(server_run, message_id, &manoeuvre_msg.data_struct) == -1) {
+                perror("error send_message()");
+                exit(EXIT_FAILURE);
+            } else {
+                printLogTitle(message_id, "sent message");
+            }
+        }
+    }
+
+    // Close the server of the agent
+    server_agent_close();
+    return 0;
+}
